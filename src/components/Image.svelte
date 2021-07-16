fce825b6e29158a2725215d185d3f962894201d3
AmiralBl3ndic <camille.briand@gmail.com>
2021-07-16 16:53:43 +0200

    feat(Image): add Image component

---
+++ src/components/Image.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+
+  export let rounded: boolean = false;
+  export let roundedCircle: boolean = false;
+  export let roundedTop: boolean = false;
+  export let roundedBottom: boolean = false;
+  export let roundedLeft: boolean = false;
+  export let roundedRight: boolean = false;
+
+  // https://www.gethalfmoon.com/static/site/img/image-1.png
+</script>
+
+<img
+  {src}
+  {alt}
+  class="img-fluid"
+  class:rounded
+  class:rounded-circle={roundedCircle}
+  class:rounded-top={roundedTop}
+  class:rounded-bottom={roundedBottom}
+  class:rounded-left={roundedLeft}
+  class:rounded-right={roundedRight}
+/>
---
+++ src/stories/Image/Image.stories.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import DarkModeWrapper from "../DarkModeWrapper.svelte";
+  import Image from "../../components/Image.svelte";
+
+  const src = "https://www.gethalfmoon.com/static/site/img/image-2.png";
+  const alt = "Plaholder image";
+</script>
+
+<Meta title="Svelte Halfmoon/Image" component={Image} />
+
+<Template let:args>
+  <DarkModeWrapper>
+    <Image {...args} />
+  </DarkModeWrapper>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    src,
+    alt,
+  }}
+/>
+
+<Story
+  name="Rounded"
+  args={{
+    src,
+    alt,
+    rounded: true,
+  }}
+/>
+
+<Story
+  name="Rounded top"
+  args={{
+    src,
+    alt,
+    roundedTop: true,
+  }}
+/>
+
+<Story
+  name="Rounded bottom"
+  args={{
+    src,
+    alt,
+    roundedBottom: true,
+  }}
+/>
+
+<Story
+  name="Rounded left"
+  args={{
+    src,
+    alt,
+    roundedLeft: true,
+  }}
+/>
+
+<Story
+  name="Rounded right"
+  args={{
+    src,
+    alt,
+    roundedRight: true,
+  }}
+/>
