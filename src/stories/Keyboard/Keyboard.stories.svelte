2d1d4306f0ed9a57d1df6481fb41abe9969a9e43
AmiralBl3ndic <camille.briand@gmail.com>
2021-07-16 16:44:06 +0200

    feat(Keyboard): add Keyboard component

---
+++ src/stories/Keyboard/Keyboard.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import DarkModeWrapper from "../DarkModeWrapper.svelte";
+  import Keyboard from "../../components/Keyboard.svelte";
+</script>
+
+<Meta title="Svelte Halfmoon/Keyboard" component={Keyboard} />
+
+<Template let:args>
+  <DarkModeWrapper>
+    <p>
+      <Keyboard>{args.content}</Keyboard>
+    </p>
+  </DarkModeWrapper>
+</Template>
+
+<Story
+  name="Keyboard"
+  args={{
+    content: "Cmd",
+  }}
+/>
